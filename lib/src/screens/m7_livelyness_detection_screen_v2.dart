a531c218e658c52cf4e0fa729cd6ab42c9f22a93
Ian Ibnu Wahab <iandroid.saimima@gmail.com>
2025-01-06 09:13:11 +0800

    update

--- lib/src/screens/m7_livelyness_detection_screen_v2.dart
+++ lib/src/screens/m7_livelyness_detection_screen_v2.dart
@@ -387,13 +387,7 @@ class _LivelynessDetectionScreenAndroidState extends State<LivelynessDetectionSc
             child: Container(
               color: Colors.white, // Warna putih di seluruh layar
               child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: buildCameraPreview()
-                  ),
-                ),
+                child: buildCameraPreview(),
               ),
             ),
           ),
